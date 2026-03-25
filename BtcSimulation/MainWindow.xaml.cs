cf99ae2df18df1e42cc1659ed3b0433bb5fc1811
fred <domaine75@gmail.com>
2026-03-25 18:26:46 +0100

    clean code

--- BtcSimulation/MainWindow.xaml.cs
+++ BtcSimulation/MainWindow.xaml.cs
@@ -1,24 +1,14 @@
 using System;
-using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Net.Http;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
-using System.IO;
 using System.Threading;
-using System.Globalization;
-using System.Windows.Threading;
+using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace BtcSimulation
 {
@@ -136,7 +126,7 @@ namespace BtcSimulation
         BtcPriceUsdText.Text = FormatPrice(prices.Usd, "en-US", "$");
         var fr = CultureInfo.GetCultureInfo("fr-FR");
         var now = DateTime.Now;
-        LastUpdatedText.Text = $"Dernière mise à jour : {now.ToString("D", fr)} à {now:HH:mm:ss}";
+        LastUpdatedText.Text = $"Dernière mise à jour :{Environment.NewLine}{now.ToString("D", fr)} à{Environment.NewLine}{now:HH:mm:ss}";
 
         Chart.AddPoint(now, prices.Eur);
       }
